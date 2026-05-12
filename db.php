fe64c6af2bda295bef190496bfdf9c6c4cc5062c
Senavor <josephsenavor251@gmail.com>
2026-05-12 16:45:48 +0000

    Use Railway DB

--- db.php
+++ db.php
@@ -1,17 +1,21 @@
 <?php
-// Database connection settings
-$host = "sql102.infinityfree.com"; 
-$user = "if0_40232781";        
-$pass = "NobleGsf1223";            
-$db   = "if0_40232781_goasodb"; 
 
-// Create connection
-$conn = new mysqli($host, $user, $pass, $db);
+$host = "yamabiko.proxy.rlwy.net";
+$port = 28539;
+$user = "root";
+$password = "iuVtfgNbpWzSSRFGEKsTePVXMgelgUek";
+$database = "railway";
 
-date_default_timezone_set('Africa/Accra');
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " .
+        $conn->connect_error);
 }
 ?>
